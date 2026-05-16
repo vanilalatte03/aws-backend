ba7ee23830152fff85159905fbc5bf377711c1ff
JH <jiho4890@gmail.com>
2026-05-16 21:32:34 +0900

    Initial commit

---
+++ src/main/java/pro/jiholim/awsbackend/AwsBackendCh4Application.java
@@ -0,0 +1,13 @@
+package pro.jiholim.awsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBackendCh4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsBackendCh4Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/jiholim/awsbackend/AwsBackendCh4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.jiholim.awsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsBackendCh4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
